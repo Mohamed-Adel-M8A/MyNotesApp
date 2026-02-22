170e144ba43a941af02b9df3331e05076c4c12c1
Mohamed-Adel-M8A <mohamedadel42013@gmail.com>
2026-02-22 20:53:05 +0200

    Rename Events.js to Events.v1.js

--- Events.v1.js
+++ Events.v1.js
@@ -1,5 +1,5 @@
-import * as UI from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/Ui.js';
-import * as Storage from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/storage.js';
+import * as UI from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/UI.js';
+import * as Storage from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/storage.v1.js';
 import * as Editor from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/Editor.js';
 import * as Exporter from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/exporter.js';
 
@@ -65,4 +65,5 @@ export function initAutoSave() {
     });
 
 }
+
 
