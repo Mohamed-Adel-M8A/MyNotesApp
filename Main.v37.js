f398af7f9b8fd52c0c3938eb9aeac48de7583a5b
Mohamed-Adel-M8A <mohamedadel42013@gmail.com>
2026-02-23 13:54:08 +0200

    Update and rename Main.v36.js to Main.v37.js

--- Main.v37.js
+++ Main.v37.js
@@ -1,4 +1,4 @@
-import * as UI from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/UI.v6.js';
+import * as UI from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/UI.v7.js';
 import * as Storage from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/storage.v2.js';
 import * as Editor from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/Editor.v3.js';
 import * as Exporter from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/exporter.js';
@@ -176,3 +176,4 @@ document.addEventListener('selectionchange', () => {
         }
     }, 200);
 });
+
