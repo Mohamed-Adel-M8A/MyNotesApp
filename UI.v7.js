2a394db5f37b8dc9a6057d9b9cc3e0597aba69b8
Mohamed-Adel-M8A <mohamedadel42013@gmail.com>
2026-02-24 17:50:12 +0200

    Update and rename UI.v6.js to UI.v7.js

--- UI.v7.js
+++ UI.v7.js
@@ -1,4 +1,4 @@
-import { saveAllCards } from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/storage.v2.js';
+import { saveAllCards } from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/storage.v3.js';
 import { startCardTimer } from 'https://cdn.jsdelivr.net/gh/Mohamed-Adel-M8A/MyNotesApp/timer.js';
 import { 
     exportSingleCardAsTxt, 
@@ -237,3 +237,4 @@ function updateTagsDataset(card) {
     card.dataset.tags = allTags.join(',').toLowerCase();
     saveAllCards();
 }
+
